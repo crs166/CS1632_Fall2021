b38d760389e4544db5e8dcfffc80f1ce5683b7b2
wonsunahn <wahn@pitt.edu>
2021-10-24 16:58:26 -0400

    Update TestRunner.java

--- exercises/4/src/TestRunner.java
+++ exercises/4/src/TestRunner.java
@@ -12,6 +12,7 @@ public class TestRunner {
 
 		// ADD ANY CLASSES YOU WISH TO TEST HERE
 		classesToTest.add(MonkeySimPinningTest.class);
+		classesToTest.add(MonkeySimStochasticTest.class);
 
 		// For all test classes added, loop through and use JUnit
 		// to run them.
@@ -23,6 +24,7 @@ public class TestRunner {
 
 			for (Failure f : r.getFailures()) {
 				System.out.println(f.toString());
+				System.out.println(f.getTrace());
 			}
 
 			// If r is not successful, there was at least one
